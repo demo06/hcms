c30a932f7e1fd357094618a36bb71d3c2f8d0a25
WenBin <demo06@126.com>
2023-10-05 17:22:31 +0800

    调整边框细节

--- lib/page/record/record_view.dart
+++ lib/page/record/record_view.dart
@@ -31,26 +31,32 @@ class _RecordPageState extends State<RecordPage> {
                 type: "选择时间",
                 onPressed: () async {
                   var result = await showDatePicker(
-                      context: context, initialDate: DateTime.now(), firstDate: DateTime(2000), lastDate: DateTime(2100));
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2100));
                   print(result.toString());
                 },
               ),
               Container(
                 color: const Color(0XFFE4E4E4),
-                child: Row(
-                  children: [
-                    Expanded(flex: 1, child: _cell(title: "房间号", isTitle: true)),
-                    Expanded(flex: 1, child: _cell(title: "房间类型", isTitle: true)),
-                    Expanded(flex: 1, child: _cell(title: "收费方式", isTitle: true)),
-                    Expanded(flex: 1, child: _cell(title: "货币类型", isTitle: true)),
-                    Expanded(flex: 1, child: _cell(title: "入住天数", isTitle: true)),
-                    Expanded(flex: 1, child: _cell(title: "单价", isTitle: true)),
-                    Expanded(flex: 1, child: _cell(title: "总计应收", isTitle: true)),
-                    Expanded(flex: 1, child: _cell(title: "支付方式", isTitle: true)),
-                    Expanded(flex: 1, child: _cell(title: "实收金额", isTitle: true)),
-                    Expanded(flex: 1, child: _cell(title: "日期", isTitle: true)),
-                    Expanded(flex: 1, child: _cell(title: "备注", isTitle: true)),
-                  ],
+                child: Container(
+                  decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 0.1)),
+                  child: Row(
+                    children: [
+                      Expanded(flex: 1, child: _cell(title: "房间号", isTitle: true)),
+                      Expanded(flex: 1, child: _cell(title: "房间类型", isTitle: true)),
+                      Expanded(flex: 1, child: _cell(title: "收费方式", isTitle: true)),
+                      Expanded(flex: 1, child: _cell(title: "货币类型", isTitle: true)),
+                      Expanded(flex: 1, child: _cell(title: "入住天数", isTitle: true)),
+                      Expanded(flex: 1, child: _cell(title: "单价", isTitle: true)),
+                      Expanded(flex: 1, child: _cell(title: "总计应收", isTitle: true)),
+                      Expanded(flex: 1, child: _cell(title: "支付方式", isTitle: true)),
+                      Expanded(flex: 1, child: _cell(title: "实收金额", isTitle: true)),
+                      Expanded(flex: 1, child: _cell(title: "日期", isTitle: true)),
+                      Expanded(flex: 1, child: _cell(title: "备注", isTitle: true)),
+                    ],
+                  ),
                 ),
               ),
               SizedBox(
@@ -81,7 +87,9 @@ class _RecordPageState extends State<RecordPage> {
         Expanded(flex: 1, child: _cell(title: record.amountPrice.toString())),
         Expanded(flex: 1, child: _cell(title: record.transType.toString())),
         Expanded(flex: 1, child: _cell(title: record.realPayAmount.toString())),
-        Expanded(flex: 1, child: _cell(title: TimeUtil().transMillToDate(millisconds: record.date.toInt(),format: 'yyyy-MM-dd'))),
+        Expanded(
+            flex: 1,
+            child: _cell(title: TimeUtil().transMillToDate(millisconds: record.date.toInt(), format: 'yyyy-MM-dd'))),
         Expanded(flex: 1, child: _cell(title: record.remark.toString())),
       ],
     );
@@ -91,7 +99,7 @@ class _RecordPageState extends State<RecordPage> {
     return Container(
         alignment: Alignment.center,
         decoration: BoxDecoration(
-            border: isTitle ? Border.all(color: Colors.black, width: 0.1) : const Border(bottom: BorderSide(color: Color(0XFFDEDEDE)))),
+        border: isTitle ? Border.all(color: Colors.black, width: 0.1) : const Border(bottom: BorderSide(color: Color(0XFFDEDEDE)))),
         child: Padding(
           padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
           child: Text(
